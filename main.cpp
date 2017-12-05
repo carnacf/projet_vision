c694b8e79d3366c01945148bb4f516f911e5f930
carnacf <florian.carnac@gmail.com>
2017-12-05 17:08:03 +0100

    Reconnaissance de cercle sur les images de la camera

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-/*#include "opencv2/opencv.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv){
     VideoCapture cap;
@@ -12,23 +17,39 @@ int main(int argc, char** argv){
           Mat frame;
           cap >> frame;
           if( frame.empty() ) break; // end of video stream
-          imshow("this is you, smile! :)", frame);
+          vector<Vec3f> coin;
+          Mat grey;
+          cvtColor(frame,grey,CV_BGR2GRAY);
+
+          HoughCircles(grey,coin,CV_HOUGH_GRADIENT,2,20,450,60,0,0 );
+
+          int l=coin.size();
+
+          //cout<<"\n The number of coins is: "<<l<<"\n\n";
+          // To draw the detected circles.
+          for( size_t i = 0; i < coin.size(); i++ ){
+          	Point center(cvRound(coin[i][0]),cvRound(coin[i][1]));
+          	// Detect center
+          	// cvRound: Rounds floating point number to nearest integer.
+          	int radius=cvRound(coin[i][2]);
+          	// To get the radius from the second argument of vector coin.
+          	circle(frame,center,3,Scalar(0,255,0),-1,8,0);
+              // circle center
+              //  To get the circle outline.
+          	circle(frame,center,radius,Scalar(0,0,255),3,8,0);
+              // circle outline
+          	//cout<< " Center location for circle "<<i+1<<" :"<<center<<"\n Diameter : "<<2*radius<<endl;
+          }
+          //cout<<endl;
+          imshow("Coin counter", frame);
           if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
     }
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
-}*/
+}
 
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-
-using namespace cv;
-using namespace std;
-
-int main(){
+/*int main(){
     Mat image;
     image=imread("IMG-20160425-WA0002.jpg",CV_LOAD_IMAGE_GRAYSCALE);
 
@@ -63,4 +84,4 @@ int main(){
     waitKey(0); // Wait for infinite time for a key press.
 
     return 0;	// Return from main function.
-}
+}*/
